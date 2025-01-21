9f66df048b98d914d354c73157f301ad1026c974
Muembiahub <jonathanmuembia3@gmail.com>
2025-01-21 16:31:22 +0200

    Fraction project

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,33 @@
 using System;
 
-class Program
+public class Fraction
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+  private int   _numerator;
+  private int   _denominator;
+  public Fraction()
+  { // Default constructor
+    _numerator = 1; // Default numerator
+    _denominator = 1;// Default denominator
+  }
+  public Fraction(int wholeNumber)
+  { // Parameterized constructor
+    _numerator = wholeNumber; // Set the numerator
+    _denominator = 1; // Set the denominator
+  }
+  // Add the parameterized constructor here
+  public Fraction(int numerator, int denominator)
+  { // Parameterized constructor
+    _numerator = numerator; // Set the numerator
+    _denominator = denominator; // Set the denominator
+  }
+  public string GetDecimalString()// Method to get the decimal value of the fraction
+    {// Method to get the decimal value of the fraction
+        string text = $"{_numerator / _denominator}";// Get the decimal value
+        return text;// Return the decimal value
     }
+    public double GetDecimalValue()
+    {// Method to get the decimal value of the fraction
+        return (double)_numerator / _denominator;// Return the decimal value
+    }
+
 }
